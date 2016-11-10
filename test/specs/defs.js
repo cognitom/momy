8997625bffff432dce243358f1540e4307628974
Tsutomu Kawamura <cognitom@gmail.com>
2016-11-10 23:05:05 +0900

    Adds tests for value conversion

---
+++ test/specs/defs.js
@@ -0,0 +1,83 @@
+/* eslint-env mocha */
+'use strict'
+
+const assert = require('assert')
+// const moment = require('moment')
+const defs = require('../../lib/defs')
+const NATIVE_TYPES = defs.NATIVE_TYPES
+
+describe('Momy Defs', () => {
+  it('BIGINT', () => {
+    const convert = NATIVE_TYPES.BIGINT.convert
+    assert.equal(convert(1), 1)
+    assert.equal(convert(1.2), 1)
+    assert.equal(convert(123456789), 123456789)
+    assert.equal(convert(undefined), 0)
+  })
+
+  it('DATE', () => {
+    const convert = NATIVE_TYPES.DATE.convert
+    assert.equal(convert(undefined), 'NULL')
+    assert.equal(convert('abcde'), 'NULL')
+    assert.equal(convert('2016-11-10'), '"2016-11-10"')
+    assert.equal(convert(1478775921696), '"2016-11-10"')
+    assert.equal(convert('1478775921696'), '"2016-11-10"')
+  })
+
+  it('DATETIME', () => {
+    const convert = NATIVE_TYPES.DATETIME.convert
+    assert.equal(convert(undefined), 'NULL')
+    assert.equal(convert('abcde'), 'NULL')
+    assert.equal(convert('2016-11-10'), '"2016-11-10 00:00:00"')
+    assert.equal(convert(1478775921696), '"2016-11-10 20:05:21"')
+    assert.equal(convert('1478775921696'), '"2016-11-10 20:05:21"')
+  })
+
+  it('DOUBLE', () => {
+    const convert = NATIVE_TYPES.DOUBLE.convert
+    assert.equal(convert(1), 1)
+    assert.equal(convert(1.2), 1.2)
+    assert.equal(convert(1.23456789), 1.23456789)
+    assert.equal(convert(undefined), 0)
+  })
+
+  it('TIME', () => {
+    const convert = NATIVE_TYPES.TIME.convert
+    assert.equal(convert(undefined), 'NULL')
+    assert.equal(convert('abcde'), 'NULL')
+    assert.equal(convert('1:23'), '"01:23:00"')
+    assert.equal(convert('12:34'), '"12:34:00"')
+    assert.equal(convert('12:34:56'), '"12:34:56"')
+    assert.equal(convert(1478775921696), '"20:05:21"')
+  })
+
+  it('TINYINT', () => {
+    const convert = NATIVE_TYPES.TINYINT.convert
+    assert.equal(convert(true), 1)
+    assert.equal(convert(false), 0)
+    assert.equal(convert(1), 1)
+    assert.equal(convert(0), 0)
+    assert.equal(convert(undefined), 0)
+  })
+
+  it('VARCHAR', () => {
+    const convert = NATIVE_TYPES.VARCHAR.convert
+    const allAscii = Array(95).map((_, i) => String.fromCharCode(32 + i)).join('')
+    const a1000 = Array(1000).map(() => 'a').join('')
+    const a255 = Array(255).map(() => 'a').join('')
+    assert.equal(convert(allAscii), `'${allAscii}'`)
+    assert.equal(convert(a1000), `'${a255}'`) // truncate
+    assert.equal(convert(undefined), "''")
+    assert.equal(convert('\x07'), "''") // skip a control char
+  })
+
+  it('TEXT', () => {
+    const convert = NATIVE_TYPES.TEXT.convert
+    const allAscii = Array(95).map((_, i) => String.fromCharCode(32 + i)).join('')
+    const a1000 = Array(1000).map(() => 'a').join('')
+    assert.equal(convert(allAscii), `'${allAscii}'`)
+    assert.equal(convert(a1000), `'${a1000}'`) // truncate
+    assert.equal(convert(undefined), "''")
+    assert.equal(convert('\x07'), "''") // skip a control char
+  })
+})
